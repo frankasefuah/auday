3d35d035b7a678a4a6ad0c097a1836e2b2014df5
Samuel Debrah <debrahsamuelkofi430@gmail.com>
2023-06-14 13:46:16 +0000

    Added all connected files

---
+++ js/countdown-custom.js
@@ -0,0 +1,13 @@
+(function($) {	
+	'use strict';
+    
+    var year = 2023;
+    var month = 8;
+    var date = 9;
+    var time = 0;
+    
+    month = month - 1;
+    jQuery(function () {
+        jQuery('#defaultCountdown').countdown({until: new Date(year, month, date, time)});
+    });
+})(jQuery);
