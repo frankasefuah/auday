888dedc047cbca54ca208c797aaff0fcd3205623
Frank Asefuah <asefuahfrank@gmail.com>
2023-06-20 10:29:40 +0000

    Updated header and countdown timer

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,14 +1,14 @@
-(function($) {	
-	'use strict';
-    
+(function ($) {
+    'use strict';
+
     var year = 2023;
-    var month = 8;
-    var date = 9;
+    var month = 11;
+    var date = 12;
     var time = 0;
-    
+
     month = month - 1;
     jQuery(function () {
-        jQuery('#defaultCountdown').countdown({until: new Date(year, month, date, time)});
+        jQuery('#defaultCountdown').countdown({ until: new Date(year, month, date, time) });
     });
 })(jQuery);
 
